4c71a942ca21a4cf37cc0562fcb9dad04b11ed67
Gerardo Andy Garcia <barcag07@icloud.com>
2025-10-22 18:59:22 -0500

    Display total amount for each person in the party
    The total gets multiplied by the tip(15%) then divided evenly by the
    total people in the party and gives the amount each person should pay.

--- tip_calculator.py
+++ tip_calculator.py
@@ -2,3 +2,7 @@ total = float(input("Enter the total amount of the bill: $"))
 party_num = int(input("How many people are in the party?: "))
 
 # Add a new stage that will ask how much the user wants to tip
+
+total_each = (total * 1.15) / party_num
+
+print(f"Each person in the party will pay ${round(total_each, 2)}")
