2ddf45d136c5a8fa71d38958ac54faffa7c072c1
Gerardo Andy Garcia <barcag07@icloud.com>
2025-10-22 18:43:29 -0500

    Ask for user input
    The user input for the number of party members and total bill amount are
    stored in variables.

---
+++ tip_calculator.py
@@ -0,0 +1,4 @@
+total = float(input("Enter the total amount of the bill: $"))
+party_num = int(input("How many people are in the party?: "))
+
+# Add a new stage that will ask how much the user wants to tip
