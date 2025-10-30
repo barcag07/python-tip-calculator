2d9da004badf47c8bebd939c7b65496f855895f2
Gerardo Andy Garcia <barcag07@icloud.com>
2025-10-30 16:48:25 -0500

    Allow user to input custom tip amount
    The user will now be able to input what percentage tip they would like
    to leave instead of the standard 15.

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,8 +1,10 @@
 total = float(input("Enter the total amount of the bill: $"))
 party_num = int(input("How many people are in the party?: "))
 
-# Add a new stage that will ask how much the user wants to tip
+tip_percentage = float(input("What percentage tip would you like to leave? %"))
 
-total_each = (total * 1.15) / party_num
+total_after_tip = (total + (1.00 * tip_percentage))
+print(f"The total bill with tip is: ${total_after_tip}")
 
+total_each = total_after_tip / party_num
 print(f"Each person in the party will pay ${round(total_each, 2)}")
